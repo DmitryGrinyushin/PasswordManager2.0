3b6ab8b085f1f4f19c5b907a6d91ee11a3c5b38e
DmitryGrinyushin <d.griniushin@gmail.com>
2025-08-03 11:32:52 +0200

    Refactor tests: update AccountManager basic operations for encryption support

--- tests/test_account_manager.cpp
+++ tests/test_account_manager.cpp
@@ -1,10 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
 #include "UserManager.h"
 #include "AccountManager.h"
 #include "DatabaseManager.h"
 #include "Logger.h"
+#include "EncryptionManager.h"
 
 TEST_CASE("AccountManager basic operations", "[account]") {
     std::string testDbPath = ":memory:"; // Database in RAM - recreated every SECTION request.
@@ -13,66 +13,77 @@ TEST_CASE("AccountManager basic operations", "[account]") {
 
     UserManager userManager(dbManager.getDb());
     int userId = userManager.registerUser("testuser", "testpass");
-    userManager.loginUser("testuser", "testpass");
-
-    AccountManager accountManager(dbManager.getDb());
-
-    SECTION("Add account") {
+    
+    AccountManager accountManager(dbManager, userManager);
+    const std::vector<unsigned char> dummyKey(32, 0x00);
+    
+    SECTION("Add account") {    
         accountManager.addAccount(userId,
-                          "testaccount",
-                          "testlogin",
-                          "testpasswordHash",
-                          "testnotes");
+                        "testaccount",
+                        "testlogin",
+                        "testpassword",
+                        "testurl",
+                        "testnotes",
+                        dummyKey);
 
-        auto accounts = accountManager.getAccountsForUser(userId);
+        auto accounts = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts.size() == 1);
         REQUIRE(accounts[0].accountName == "testaccount");
         REQUIRE(accounts[0].login == "testlogin");
-        REQUIRE(accounts[0].passwordHash == "testpasswordHash");
+        REQUIRE_FALSE(accounts[0].encryptedPassword.empty());
+        REQUIRE(accounts[0].url == "testurl");
         REQUIRE(accounts[0].notes == "testnotes");
     }
 
     SECTION("Update account") {
         accountManager.addAccount(userId,
-                          "testaccount",
-                          "testlogin",
-                          "testpasswordHash",
-                          "testnotes");
+                        "testaccount",
+                        "testlogin",
+                        "testpassword",
+                        "testurl",
+                        "testnotes",
+                        dummyKey);
 
-        auto accounts = accountManager.getAccountsForUser(userId);
+        auto accounts = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts.size() == 1);
 
         int accountId = accounts[0].id;
 
-        accountManager.updateAccount(accountId,
-                          "testaccount_1",
-                          "testlogin_1",
-                          "testpasswordHash_1",
-                          "testnotes_1");
+        accountManager.updateAccount(userId,
+                        accountId,
+                        "testaccount_1",
+                        "testlogin_1",
+                        "testpassword_1",
+                        "testurl_1",
+                        "testnotes_1",
+                        dummyKey);
 
-        auto accounts_1 = accountManager.getAccountsForUser(userId);
+        auto accounts_1 = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts_1.size() == 1);
         REQUIRE(accounts_1[0].accountName == "testaccount_1");
         REQUIRE(accounts_1[0].login == "testlogin_1");
-        REQUIRE(accounts_1[0].passwordHash == "testpasswordHash_1");
+        REQUIRE_FALSE(accounts_1[0].encryptedPassword.empty());
+        REQUIRE(accounts_1[0].url == "testurl_1");
         REQUIRE(accounts_1[0].notes == "testnotes_1");
     }
 
     SECTION("Delete account") {
         accountManager.addAccount(userId,
-                          "testaccount",
-                          "testlogin",
-                          "testpasswordHash",
-                          "testnotes");
+                        "testaccount",
+                        "testlogin",
+                        "testpassword",
+                        "testurl",
+                        "testnotes",
+                        dummyKey);
 
-        auto accounts = accountManager.getAccountsForUser(userId);
+        auto accounts = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts.size() == 1);
 
         int accountId = accounts[0].id;
 
-        accountManager.deleteAccount(accountId);
-        auto accounts_1 = accountManager.getAccountsForUser(userId);
-        REQUIRE(accounts_1.size() == 0);
+        accountManager.deleteAccount(userId, accountId);
+        auto accounts_1 = accountManager.getAccountsForUser(userId, dummyKey);
+        REQUIRE(accounts_1.empty());
     }
 
     SECTION("Add accounts for multiple users") {
@@ -80,105 +91,125 @@ TEST_CASE("AccountManager basic operations", "[account]") {
         userManager.loginUser("seconduser", "secondpass");
 
         accountManager.addAccount(userId,
-                          "testaccount_1",
-                          "testlogin_1",
-                          "testpasswordHash_1",
-                          "testnotes_1");
+                        "testaccount_1",
+                        "testlogin_1",
+                        "testpassword_1",
+                        "testurl_1",
+                        "testnotes_1",
+                        dummyKey);
 
+        const std::vector<unsigned char> dummyKey_2(32, 0x00);
         accountManager.addAccount(userId_2,
-                          "testaccount_2",
-                          "testlogin_2",
-                          "testpasswordHash_2",
-                          "testnotes_2");
+                        "testaccount_2",
+                        "testlogin_2",
+                        "testpassword_2",
+                        "testurl_2",
+                        "testnotes_2",
+                        dummyKey_2);
 
-        auto accounts_1 = accountManager.getAccountsForUser(userId);
-        auto accounts_2 = accountManager.getAccountsForUser(userId_2);
+        auto accounts_1 = accountManager.getAccountsForUser(userId, dummyKey);
+        auto accounts_2 = accountManager.getAccountsForUser(userId_2, dummyKey_2);
 
         REQUIRE(accounts_1.size() == 1);
         REQUIRE(accounts_2.size() == 1);
 
         REQUIRE(accounts_1[0].accountName == "testaccount_1");
         REQUIRE(accounts_1[0].login == "testlogin_1");
-        REQUIRE(accounts_1[0].passwordHash == "testpasswordHash_1");
+        REQUIRE_FALSE(accounts_1[0].encryptedPassword.empty());
+        REQUIRE(accounts_1[0].url == "testurl_1");
         REQUIRE(accounts_1[0].notes == "testnotes_1");
 
         REQUIRE(accounts_2[0].accountName == "testaccount_2");
         REQUIRE(accounts_2[0].login == "testlogin_2");
-        REQUIRE(accounts_2[0].passwordHash == "testpasswordHash_2");
+        REQUIRE_FALSE(accounts_2[0].encryptedPassword.empty());
+        REQUIRE(accounts_2[0].url == "testurl_2");
         REQUIRE(accounts_2[0].notes == "testnotes_2");
     }
     
     SECTION("Add multiple accounts for a single user") {
         accountManager.addAccount(userId,
-                          "testaccount_1",
-                          "testlogin_1",
-                          "testpasswordHash_1",
-                          "testnotes_1");
+                        "testaccount_1",
+                        "testlogin_1",
+                        "testpassword_1",
+                        "testurl_1",
+                        "testnotes_1",
+                        dummyKey);
 
         accountManager.addAccount(userId,
-                          "testaccount_2",
-                          "testlogin_2",
-                          "testpasswordHash_2",
-                          "testnotes_2");
+                        "testaccount_2",
+                        "testlogin_2",
+                        "testpassword_2",
+                        "testurl_2",
+                        "testnotes_2",
+                        dummyKey);
 
-        auto accounts = accountManager.getAccountsForUser(userId);
+        auto accounts = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts.size() == 2);
 
         REQUIRE(accounts[0].accountName == "testaccount_1");
         REQUIRE(accounts[0].login == "testlogin_1");
-        REQUIRE(accounts[0].passwordHash == "testpasswordHash_1");
+        REQUIRE_FALSE(accounts[0].encryptedPassword.empty());
+        REQUIRE(accounts[0].url == "testurl_1");
         REQUIRE(accounts[0].notes == "testnotes_1");
 
         REQUIRE(accounts[1].accountName == "testaccount_2");
         REQUIRE(accounts[1].login == "testlogin_2");
-        REQUIRE(accounts[1].passwordHash == "testpasswordHash_2");
+        REQUIRE_FALSE(accounts[1].encryptedPassword.empty());
+        REQUIRE(accounts[1].url == "testurl_2");
         REQUIRE(accounts[1].notes == "testnotes_2");
     }
 
     SECTION("Update non-existent account") {
         accountManager.addAccount(userId,
-                          "testaccount",
-                          "testlogin",
-                          "testpasswordHash",
-                          "testnotes");
+                        "testaccount",
+                        "testlogin",
+                        "testpassword",
+                        "testurl",
+                        "testnotes",
+                        dummyKey);
 
-        auto accounts = accountManager.getAccountsForUser(userId);
+        auto accounts = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts.size() == 1);
 
         int accountId = accounts[0].id;
 
-        accountManager.deleteAccount(accountId);
-        auto accounts_1 = accountManager.getAccountsForUser(userId);
+        accountManager.deleteAccount(userId, accountId);
+        auto accounts_1 = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts_1.size() == 0);
 
-        REQUIRE_THROWS_AS(accountManager.updateAccount(accountId,
-                          "testaccount",
-                          "testlogin",
-                          "testpasswordHash",
-                          "testnotes"), std::runtime_error);
+        REQUIRE_THROWS_AS(accountManager.updateAccount(userId,
+                        accountId,
+                        "testaccount",
+                        "testlogin",
+                        "testpassword",
+                        "testurl",
+                        "testnotes",
+                        dummyKey), std::runtime_error);
     }
 
     SECTION("Delete non-existent account") {
         accountManager.addAccount(userId,
-                          "testaccount",
-                          "testlogin",
-                          "testpasswordHash",
-                          "testnotes");
+                        "testaccount",
+                        "testlogin",
+                        "testpasswordHash",
+                        "testurl",
+                        "testnotes",
+                        dummyKey);
 
-        auto accounts = accountManager.getAccountsForUser(userId);
+        auto accounts = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts.size() == 1);
 
         int accountId = accounts[0].id;
 
-        accountManager.deleteAccount(accountId);
-        auto accounts_1 = accountManager.getAccountsForUser(userId);
+        accountManager.deleteAccount(userId, accountId);
+        auto accounts_1 = accountManager.getAccountsForUser(userId, dummyKey);
         REQUIRE(accounts_1.size() == 0);
         
-        REQUIRE_THROWS_AS(accountManager.deleteAccount(accountId), std::runtime_error);
+        REQUIRE_THROWS_AS(accountManager.deleteAccount(userId, accountId), std::runtime_error);
     }
 
     SECTION("Get accounts for non-existent user") {
-        auto accounts = accountManager.getAccountsForUser(99999);
+        auto accounts = accountManager.getAccountsForUser(99999, dummyKey);
         REQUIRE(accounts.empty());
     }
 }
