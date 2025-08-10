9f8616120dcb9f7459a02c0e989b791c8da7ee92
DmitryGrinyushin <d.griniushin@gmail.com>
2025-08-10 15:05:32 +0200

    Add POST endpoint: register user

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -1,4 +1,5 @@
 #include "WebServer.h"
+#include "json.hpp"
 #include <iostream>
 
 WebServer::WebServer(std::string& host, int port, UserManager& userManager)
@@ -9,8 +10,37 @@ void WebServer::setupRoutes() {
         res.set_content("Password Manager Web API is running!", "text/plain");
     });
 
+    server_.Post("/register", [this] (const httplib::Request& req, httplib::Response& res) {
+        try {
+            auto jsonBody = nlohmann::json::parse(req.body);
+
+            if (!jsonBody.contains("username") || !jsonBody.contains("password")) {
+                res.status = 400; // Bad Request
+                res.set_content(R"({"error": "Missing username or password"})", "application/json");
+                return;
+            }
+
+            std::string username = jsonBody["username"];
+            std::string password = jsonBody["password"];
+
+            try {
+                userManager_.registerUser(username, password);
+                res.status = 201; // Created
+                res.set_content(R"({"message": "User registered successfully"})", "application/json");
+            } catch (const std::exception& e) {
+                res.status = 409; // Conflict (user already exists)
+                std::string errorJson = std::string(R"({"error": ")") + e.what() + "\"}";
+                res.set_content(errorJson, "application/json");
+            }
+        }
+        catch (const std::exception& e) {
+            res.status = 400;
+            res.set_content(R"({"error": "Invalid JSON"})", "application/json");
+            return;
+        }
+    });
+
     // server_.Post("/login", ...);
-    // server_.Post("/register", ...);
 }
 
 void WebServer::start() {
